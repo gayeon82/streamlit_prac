2c0fe1249bc91b5764b9507d96289f7f0fe3fab1
gayeon82 <125945444+gayeon82@users.noreply.github.com>
2023-03-10 11:15:26 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+import streamlit as st
+
+st.title('streamlit test!')
+
+tab1, tab2= st.tabs(['Tab A' , 'Tab B'])
+    
+with tab1:
+  #tab A 를 누르면 표시될 내용
+  st.write('merong')
+    
+with tab2:
+  #tab B를 누르면 표시될 내용 
+  st.write('hi')
